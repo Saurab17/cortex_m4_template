fd9651842204752ea33c7dd85452ef0a3414343c
saurabh <saurabh.yadavx@gmail.com>
2020-09-19 23:53:14 +0530

    integrated driverlib into the project

--- ti_msp432/main_blinky.c
+++ ti_msp432/main_blinky.c
@@ -1,5 +1,10 @@
-/* --COPYRIGHT--,BSD_EX
- * Copyright (c) 2013, Texas Instruments Incorporated
+/*
+ * -------------------------------------------
+ *    MSP432 DriverLib - v3_21_00_05 
+ * -------------------------------------------
+ *
+ * --COPYRIGHT--,BSD,BSD
+ * Copyright (c) 2016, Texas Instruments Incorporated
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -28,51 +33,87 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
  * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *******************************************************************************
- *
- *                       MSP432 CODE EXAMPLE DISCLAIMER
- *
- * MSP432 code examples are self-contained low-level programs that typically
- * demonstrate a single peripheral function or device feature in a highly
- * concise manner. For this the code may rely on the device's power-on default
- * register values and settings such as the clock configuration and care must
- * be taken when combining code from several examples to avoid potential side
- * effects. Also see http://www.ti.com/tool/mspdriverlib for an API functional
- * library & https://dev.ti.com/pinmux/ for a GUI approach to peripheral configuration.
- *
  * --/COPYRIGHT--*/
-//******************************************************************************
-//   MSP432P401 Demo - Software Toggle P1.0
-//
-//   Description: Toggle P1.0 by xor'ing P1.0 inside of a software loop.
-//   ACLK = 32.768kHz, MCLK = SMCLK = default DCO~1MHz
-//
-//                MSP432p401rpz
-//             -----------------
-//         /|\|                 |
-//          | |                 |
-//          --|RST              |
-//            |                 |
-//            |             P1.0|-->LED
-//
-//   Dung Dang
-//   Texas Instruments Inc.
-//   October 2015 (updated) | November 2013 (created)
-//   Built with Code Composer Studio V6.0
-//******************************************************************************
-#include "msp.h"
+/*******************************************************************************
+ * MSP432 Timer32 - Periodic LED Blink
+ *
+ * Description: Starts timer on a button press, toggles GPIO/LED for
+ * a timer period, and then turns off LED at end of period.
+ *
+ *                MSP432P401
+ *             ------------------
+ *         /|\|                  |
+ *          | |                  |
+ *          --|RST         P1.1  |<--- Switch
+ *            |                  |
+ *            |                  |
+ *            |            P1.0  |---> LED
+ *            |                  |
+ *            |                  |
+ *
+ * Author: Timothy Logan
+ *******************************************************************************/
+/* DriverLib Includes */
+#include "driverlib.h"
+
+/* Standard Includes */
 #include <stdint.h>
 
-int main(void) {
-    volatile uint32_t i;
+#include <stdbool.h>
 
-    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;               // Stop WDT
-    P1->DIR |= BIT0;                          // P1.0 set as output
+int main(void)
+{
+    /* Halting the Watchdog */
+    MAP_WDT_A_holdTimer();
 
-    while (1)                               // continuous loop
+    /* Setting MCLK to REFO at 128Khz for LF mode */
+    MAP_CS_setReferenceOscillatorFrequency(CS_REFO_128KHZ);
+    MAP_CS_initClockSignal(CS_MCLK, CS_REFOCLK_SELECT, CS_CLOCK_DIVIDER_1);
+    MAP_PCM_setPowerState(PCM_AM_LF_VCORE0);
+
+    /* Configuring GPIO */
+    MAP_GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
+    MAP_GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
+    MAP_GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN1);
+    MAP_GPIO_clearInterruptFlag(GPIO_PORT_P1, GPIO_PIN1);
+    MAP_GPIO_enableInterrupt(GPIO_PORT_P1, GPIO_PIN1);
+
+    /* Configuring Timer32 to 128000 (1s) of MCLK in periodic mode */
+    MAP_Timer32_initModule(TIMER32_BASE, TIMER32_PRESCALER_1, TIMER32_32BIT,
+            TIMER32_PERIODIC_MODE);
+
+    /* Enabling interrupts */
+    MAP_Interrupt_enableInterrupt(INT_PORT1);
+    MAP_Interrupt_enableInterrupt(INT_T32_INT1);
+    MAP_Interrupt_enableMaster();
+
+    /* Sleeping when not in use */
+    while (1)
     {
-        P1->OUT ^= BIT0;                      // Blink P1.0 LED
-        for (i = 20000; i > 0; i--);        // Delay
+        MAP_PCM_gotoLPM0();
     }
+}
+
+/* GPIO ISR */
+void PORT1_IRQHandler(void)
+{
+    uint32_t status = MAP_GPIO_getEnabledInterruptStatus(GPIO_PORT_P1);
+    MAP_GPIO_clearInterruptFlag(GPIO_PORT_P1,status);
+
+    if (GPIO_PIN1 & status)
+    {
+        MAP_GPIO_disableInterrupt(GPIO_PORT_P1, GPIO_PIN1);
+        MAP_GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0);
+        MAP_Timer32_setCount(TIMER32_BASE,128000);
+        MAP_Timer32_enableInterrupt(TIMER32_BASE);
+        MAP_Timer32_startTimer(TIMER32_BASE, true);
+    }
+}
+
+/* Timer32 ISR */
+void T32_INT1_IRQHandler(void)
+{
+    MAP_Timer32_clearInterruptFlag(TIMER32_BASE);
+    MAP_GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
+    MAP_GPIO_enableInterrupt(GPIO_PORT_P1, GPIO_PIN1);
 }
